9fefe1e61cc59ad0735bc951395f30d2fe8df4fb
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-07-17 08:46:13 +0530

    feat: Refactor layout to include session provider and toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
-import type { Metadata } from "next";
+'use client';
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { Toaster } from "@/components/ui/sonner";
+
+import { SessionProvider } from 'next-auth/react';
+import { metadata } from "./metadata";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Chitrakosha - Artwork Marketplace",
-  description:
-    "A marketplace for artists to sell and art lovers to buy unique artworks.",
-};
 
 export default function RootLayout({
   children,
@@ -21,18 +20,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="flex flex-col min-h-screen">
-            <Navbar />
-            <main className="flex-grow">{children}</main>
-            <Footer />
-          </div>
-        </ThemeProvider>
+        <SessionProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex flex-col min-h-screen">
+              <Navbar />
+              <main className="flex-grow">{children}</main>
+              <Footer />
+              <Toaster />
+            </div>
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
